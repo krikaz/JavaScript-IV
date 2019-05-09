afc514a5fbdf6da9fa2de730acadc1865a1b166b
krikaz <pascal.bou@protonmail.com>
2019-05-09 14:21:14 +0200

    created 3 objects per class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,3 +1,5 @@
+// classes
+
 class Person {
   constructor(obj) {
     this.name = obj.name;
@@ -60,3 +62,94 @@ class ProjectManager extends Instructor {
     console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
   }
 }
+
+
+
+// objects creation
+
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+const jack = new Instructor({
+  name: 'Jack',
+  location: 'Hammer',
+  age: 24,
+  gender: 'male',
+  favLanguage: 'Go',
+  specialty: 'Back-end',
+  catchPhrase: 'Do it now'
+});
+
+const rose = new Instructor({
+  name: 'Rose',
+  location: 'Redtree',
+  age: 21,
+  gender: 'female',
+  favLanguage: 'Python',
+  specialty: 'Full Stack',
+  catchPhrase: 'Fully fuller'
+});
+
+const johnny = new Instructor({
+  name: 'Johnny',
+  location: 'Little Town',
+  age: 36,
+  gender: 'male',
+  previousBackground: 'Marine',
+  className: 'WEBEU2',
+  favSubjects: ['Html', 'CSS', 'JavaScript']
+});
+
+const jane = new Instructor({
+  name: 'Jane',
+  location: 'Deadwood',
+  age: 28,
+  gender: 'female',
+  previousBackground: 'Poker Player',
+  className: 'WEBEU2',
+  favSubjects: ['Go', 'C++', 'C']
+});
+
+const maria = new Instructor({
+  name: 'Maria',
+  location: 'Seattle',
+  age: 27,
+  gender: 'female',
+  previousBackground: 'Sniper',
+  className: 'WEBEU2',
+  favSubjects: ['Haskell', 'Java']
+});
+
+const aletta = new Instructor({
+  name: 'Aletta',
+  location: 'Singapore',
+  age: 33,
+  gender: 'female',
+  gradClassName: 'CS50',
+  favInstructor: 'Dan'
+});
+
+const ashley = new Instructor({
+  name: 'Ashley',
+  location: 'Cleveland',
+  age: 29,
+  gender: 'female',
+  gradClassName: 'CS50',
+  favInstructor: 'Jackie'
+});
+
+const vince = new Instructor({
+  name: 'Vince',
+  location: 'Riga',
+  age: 25,
+  gender: 'male',
+  gradClassName: 'CS50',
+  favInstructor: 'Brian'
+});
